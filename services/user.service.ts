0ed2ec3bae8c9d3c8a6e86456d35d0cb9ea1e3dc
Caio Silva <caiossantosp5560@gmail.com>
2023-11-07 17:27:21 -0300

    chore: get user data with redis

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,10 +1,14 @@
 import { Response } from 'express';
+import { redis } from '../utils/redis';
 import { userModel } from '../models/user.model';
 
 export const getUserById = async(id: string, res: Response) => {
-	const user = await userModel.findById(id);
-	res.status(200).json({
-		status: 'success',
-		user
-	});
-}
+	const userJson = await redis.get(id);
+
+	if(userJson){
+		const user = JSON.parse(userJson);
+		res.status(200).json({
+			status: 'success',
+			user
+		});
+	};}
