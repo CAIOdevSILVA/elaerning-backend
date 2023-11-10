c57c3b6b0ad4375baa0366b3605f863a3d9a00a5
Caio Silva <caiossantosp5560@gmail.com>
2023-11-10 16:38:09 -0300

    feat: add uploadCourse to create courses

---
+++ controllers/course.controller.ts
@@ -0,0 +1,53 @@
+import { Request, Response, NextFunction } from 'express';
+import { CatchAsyncErrors } from '../middleware/catchAsyncErrors';
+import ErrorHandler from '../utils/ErrorHandler';
+import cloudinary from 'cloudinary';
+import { createCourse } from '../services/course.service';
+
+//upload course
+export const uploadCourse = CatchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+	try {
+		const data = req.body;
+		const thumbnail = data.thumbnail;
+
+		if(thumbnail){
+			const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+				folder: 'courses'
+			});
+
+			data.thumbnail = {
+				public_id: myCloud.public_id,
+				url: myCloud.secure_url,
+			};
+		};
+
+		createCourse(data, res, next);
+	} catch (error: any) {
+		return next(new ErrorHandler(error.message, 400))
+	}
+});
+
+//edit course
+export const editCours = CatchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+	try {
+		const data = req.body;
+		const thumbnail = data.thumbnail;
+
+		if(thumbnail){
+			await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+
+			const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+				folder: 'courses'
+			});
+
+			data.thumbnail = {
+				public_id: myCloud.public_id,
+				url: myCloud.secure_url,
+			};
+		};
+
+		
+	} catch (error: any) {
+		return next(new ErrorHandler(error.message, 400));
+	};
+});
