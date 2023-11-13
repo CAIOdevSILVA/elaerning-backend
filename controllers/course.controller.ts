3f9f61a83da94d8958f54ff246da29b92c016752
Caio Silva <caiossantosp5560@gmail.com>
2023-11-13 17:12:25 -0300

    feat: add addAnswerToQuestion function and send email to user when the question is replied

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -6,12 +6,23 @@ import { createCourse } from "../services/course.service";
 import { courseModel } from "../models/course.model";
 import { redis } from "../utils/redis";
 import mongoose from "mongoose";
+import path from 'path';
+import ejs from 'ejs';
+import sendMailToUser from '../utils/sendMails';
 
 //Add question in course
 interface IAddQuestionData {
 	question: string;
 	courseId: string;
 	contentId: string;
+}
+
+//Add answer in question
+interface IAddAnswerData {
+	answer: string;
+	courseId: string;
+	contentId: string;
+	questionId: string;
 }
 
 //upload course
@@ -190,6 +201,10 @@ export const addQuestion = CatchAsyncErrors(
 				item._id.equals(contentId)
 			);
 
+			if (!courseContent) {
+				return next(new ErrorHandler("Invalid contentId", 400));
+			}
+
 			//create a new question object
 			const newQuestion: any = {
 				user: req.user,
@@ -214,5 +229,70 @@ export const addQuestion = CatchAsyncErrors(
 );
 
 //add answer to question in course
+export const addAnswerInQuestion = CatchAsyncErrors(async(req: Request, res: Response, next:NextFunction) => {
+	try {
+		const { answer, courseId, contentId, questionId }: IAddAnswerData = req.body;
+		const course = await courseModel.findById(courseId);
 
+		if (!mongoose.Types.ObjectId.isValid(contentId)) {
+			return next(new ErrorHandler("Invalid contentId", 400));
+		}
+
+		const courseContent = course?.courseData.find((item: any) =>
+			item._id.equals(contentId)
+		);
+
+		if (!courseContent) {
+			return next(new ErrorHandler("Invalid contentId", 400));
+		};
+
+		const question = courseContent?.questions.find((item: any) => item._id.equals(questionId));
+
+		if (!question) {
+			return next(new ErrorHandler("Invalid contentId", 400));
+		};
+
+		//add answer object
+		const newAnswer: any = {
+			user: req.user,
+			answer,
+		};
+
+		//add this answer in our course content
+		question?.questionReplies.push(newAnswer);
+
+		//save updated question in our db
+		await course?.save();
+
+		if(req.user?._id === question.user._id){
+			//create a notification
+		} else{
+			//send a email to user
+			const data = {
+				name: question.user.name,
+				title: courseContent.title
+			};
+
+			const html = await ejs.renderFile(path.join(__dirname, '../mails/question-reply.ejs'), data);
+
+			try {
+				await sendMailToUser({
+					email: question.user.email,
+					subject: 'Question Reply',
+					template: 'question-reply-ejs',
+					data,
+				});
+			} catch (error: any) {
+				return next(new ErrorHandler(error.message, 501));
+			}
+		};
+
+		res.status(200).json({
+			success: true,
+			course
+		});
+	}	catch (error: any) {
+		return next(new ErrorHandler(error.message, 500));
+	}
+});
 
--- routes/course.routes.ts
+++ routes/course.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addQuestion, editCourse, getAllCourse, getCourseByUser, getSingleCourse, uploadCourse } from '../controllers/course.controller';
+import { addAnswerInQuestion, addQuestion, editCourse, getAllCourse, getCourseByUser, getSingleCourse, uploadCourse } from '../controllers/course.controller';
 import { isAuthenticated, authorizedRoles } from '../middleware/auth';
 
 const courseRouter = express.Router();
@@ -10,5 +10,6 @@ courseRouter.get('/get-course/:id', getSingleCourse);
 courseRouter.get('/get-courses', getAllCourse);
 courseRouter.get('/get-course-content/:id', isAuthenticated,getCourseByUser);
 courseRouter.put('/add-question', isAuthenticated,addQuestion);
+courseRouter.put('/add-answer', isAuthenticated,addAnswerInQuestion);
 
 export default courseRouter;
