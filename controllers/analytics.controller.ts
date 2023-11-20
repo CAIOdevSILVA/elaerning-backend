32f83dd2a775d779b1c2b8a5859d372c1154d420
Caio Silva <caiossantosp5560@gmail.com>
2023-11-20 16:21:54 -0300

    feat: add courses and orders analytics

--- controllers/analytics.controller.ts
+++ controllers/analytics.controller.ts
@@ -3,8 +3,10 @@ import { CatchAsyncErrors } from '../middleware/catchAsyncErrors';
 import ErrorHandler from '../utils/ErrorHandler';
 import { generateLast12MonthData } from '../utils/analytics.generator';
 import { userModel } from '../models/user.model';
+import { courseModel } from '../models/course.model';
+import { orderModel } from '../models/order.model';
 
-//get users analytics --only for users
+//get users analytics --only for admin
 export const getUsersAnalytics = CatchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
 	try {
 		const users = await generateLast12MonthData(userModel);
@@ -12,6 +14,34 @@ export const getUsersAnalytics = CatchAsyncErrors(async(req: Request, res: Respo
 		res.status(200).json({
 			success: true,
 			users,
+		});
+	} catch (error: any) {
+		return next(new ErrorHandler(error.message, 500));
+	}
+});
+
+//get course analytics --only for admin
+export const getCoursesAnalytics = CatchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+	try {
+		const courses = await generateLast12MonthData(courseModel);
+
+		res.status(200).json({
+			success: true,
+			courses,
+		});
+	} catch (error: any) {
+		return next(new ErrorHandler(error.message, 500));
+	}
+});
+
+//get orders analytics --only for admin
+export const getOrdersAnalytics = CatchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+	try {
+		const orders = await generateLast12MonthData(orderModel);
+
+		res.status(200).json({
+			success: true,
+			orders,
 		});
 	} catch (error: any) {
 		return next(new ErrorHandler(error.message, 500));
